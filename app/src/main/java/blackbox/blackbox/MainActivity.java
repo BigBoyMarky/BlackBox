ae436c435b04a84a49f83027bb39a9edbdf05729
Mark Yang <markyang13705@gmail.com>
2016-02-27 12:26:47 -0500

    modified and wrapped layouts, added settings FAB and also account icon

--- app/src/main/java/blackbox/blackbox/MainActivity.java
+++ app/src/main/java/blackbox/blackbox/MainActivity.java
@@ -1,7 +1,9 @@
 package blackbox.blackbox;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton settingsFAB = (FloatingActionButton) findViewById(R.id.settingsFAB);
+        settingsFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // open up or close all of the settings
+            }
+        });
     }
 }
