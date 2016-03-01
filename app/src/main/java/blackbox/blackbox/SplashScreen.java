ed98cfb8ca732388c6c15478a9377c4342c98251
Mark Yang <markyang13705@gmail.com>
2016-02-29 21:59:14 -0500

    added a simple splash screen

---
+++ app/src/main/java/blackbox/blackbox/SplashScreen.java
@@ -0,0 +1,35 @@
+package blackbox.blackbox;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try {
+                    sleep(1300);
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
