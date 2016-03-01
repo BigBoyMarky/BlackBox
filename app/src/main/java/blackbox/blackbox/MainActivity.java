ebfe3db62acf33ad81fe7a6713a619903bb9df19
Mark Yang <markyang13705@gmail.com>
2016-02-29 23:09:44 -0500

    adding the quit button

--- app/src/main/java/blackbox/blackbox/MainActivity.java
+++ app/src/main/java/blackbox/blackbox/MainActivity.java
@@ -4,20 +4,52 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean isSettingsOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("Creating main activity");
 
         FloatingActionButton settingsFAB = (FloatingActionButton) findViewById(R.id.settingsFAB);
         settingsFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // open up or close all of the settings
+                System.out.println("settingsFAB is clicked");
+                if (isSettingsOpen) {
+                    View settingsQuitView = findViewById(R.id.settingsQuit);
+                    RelativeLayout.MarginLayoutParams params = (RelativeLayout.MarginLayoutParams) settingsQuitView.getLayoutParams();
+                    params.setMargins(0, 0, 0, 0);
+                    v.requestLayout();
+                    isSettingsOpen = !isSettingsOpen;
+                    System.out.println("isSettingsOpen");
+                } else { // settings is closed so need to open it
+                    View settingsQuitView = findViewById(R.id.settingsQuit);
+                    //RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) settingsQuitView.getLayoutParams();
+                    RelativeLayout.MarginLayoutParams params = (RelativeLayout.MarginLayoutParams) settingsQuitView.getLayoutParams();
+                    params.setMargins(0, 0, 230, 0);
+                    v.requestLayout();
+                    isSettingsOpen = !isSettingsOpen;
+                    System.out.println("!isSettingsOpen");
+                }
             }
         });
+
+        FloatingActionButton settingsQuit = (FloatingActionButton) findViewById(R.id.settingsQuit);
+        settingsQuit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Quitting application");
+                finish();
+            }
+        });
+        /*getActivity().finish();
+        System.exit(0);*/
     }
 }
