3337a5e267c2a45f68ff5029b42e4b7b681a80f9
Mark Yang <markyang13705@gmail.com>
2016-03-01 23:45:32 -0500

    added some comments and removed some extras

--- app/src/main/java/blackbox/blackbox/MainActivity.java
+++ app/src/main/java/blackbox/blackbox/MainActivity.java
@@ -4,43 +4,47 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean isSettingsOpen = false;
+    boolean isSettingsOpen = false; // flag to toggle the settings menu
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Creating main activity");
 
+        // settings menu
         FloatingActionButton settingsFAB = (FloatingActionButton) findViewById(R.id.settingsFAB);
         settingsFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("settingsFAB is clicked");
+                // settings is open so need to close it
                 if (isSettingsOpen) {
                     View settingsQuitView = findViewById(R.id.settingsQuit);
                     RelativeLayout.MarginLayoutParams params = (RelativeLayout.MarginLayoutParams) settingsQuitView.getLayoutParams();
                     params.setMargins(0, 0, 0, 0);
                     v.requestLayout();
                     isSettingsOpen = !isSettingsOpen;
-                    System.out.println("isSettingsOpen");
-                } else { // settings is closed so need to open it
+                    //System.out.println("isSettingsOpen");
+                }
+                // settings is closed so need to open it
+                else {
                     View settingsQuitView = findViewById(R.id.settingsQuit);
-                    //RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) settingsQuitView.getLayoutParams();
                     RelativeLayout.MarginLayoutParams params = (RelativeLayout.MarginLayoutParams) settingsQuitView.getLayoutParams();
                     params.setMargins(0, 0, 230, 0);
                     v.requestLayout();
                     isSettingsOpen = !isSettingsOpen;
-                    System.out.println("!isSettingsOpen");
+                    //System.out.println("!isSettingsOpen");
                 }
             }
         });
 
+        // quit button within settings menu
         FloatingActionButton settingsQuit = (FloatingActionButton) findViewById(R.id.settingsQuit);
         settingsQuit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,7 +53,5 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
-        /*getActivity().finish();
-        System.exit(0);*/
     }
 }
